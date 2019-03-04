648ecc5ae43992bb5d224a76fa1b1b9460a3e9a9
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-03-05 01:47:52 +0900

    change

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
-docker exec -ti $MY_NAME bash -c "apt install -y time wget build-essential git pkg-config libssl-dev python3-pytest" > /dev/null
+docker exec -ti $MY_NAME bash -c "apt install -y
+    time wget build-essential git pkg-config libssl-dev python3-pytest"
 # docker exec -ti $MY_NAME bash -c "apt install -y libsodium-dev" > /dev/null
 docker exec -ti $MY_NAME bash -c "set -ev;
     wget https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz;
