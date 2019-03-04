32d9ac802f4913e757751cae1378b69bbdbabd9a
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-03-05 01:14:13 +0900

    update

--- travis-ci/script_linux.bash
+++ travis-ci/script_linux.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
-set -ex
+set -ev
 if [ "cpp" = "$MY_MODE" ]; then
-    docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT;
+    docker exec -ti $MY_NAME bash -c "set -ev; cd $MY_TARGET_MOUNT;
         env CPATH=$MY_CPATH make CXX=$CXX;
         ls /usr/local/lib;
         make check"
 elif [ "python" = "$MY_MODE" ]; then
-    docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; py.test-3"
+    docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT;
+        py.test-3"
 else
     echo skip
 fi
