1583e27c53756b5bf3639850de392fb54127d62b
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-03-04 22:58:48 +0900

    fix

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -4,4 +4,3 @@ docker exec -ti $MY_NAME bash -c "apt install -y time wget build-essential git p
 # docker exec -ti $MY_NAME bash -c "apt install -y libsodium-dev" > /dev/null
 docker exec -ti $MY_NAME bash -c "wget https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz; tar xf libsodium-1.0.17.tar.gz; cd libsodium-1.0.17; ./configure; make -j; make -j install" > /dev/null
 docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; git clone https://github.com/herumi/cybozulib.git" > /dev/null
-docker exec -ti $MY_NAME bash -c "export CPATH=$MY_CPATH" > /dev/null
--- travis-ci/script_linux.bash
+++ travis-ci/script_linux.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 if [ "cpp" = "$MY_MODE" ]; then
-    docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; make CXX=$CXX"
+    docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; env CPATH=$MY_CPATH make CXX=$CXX"
     docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; make check"
 elif [ "python" = "$MY_MODE" ]; then
     docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; py.test-3"
