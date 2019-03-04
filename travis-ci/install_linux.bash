d34e7f2aa37d8edf5a1930f9ac238cc81405ae7f
Tadanori TERUYA <tadanori.teruya@gmail.com>
2019-03-05 01:38:52 +0900

    udpate

--- travis-ci/install_linux.bash
+++ travis-ci/install_linux.bash
@@ -2,5 +2,12 @@
 set -ev
 docker exec -ti $MY_NAME bash -c "apt install -y time wget build-essential git pkg-config libssl-dev python3-pytest" > /dev/null
 # docker exec -ti $MY_NAME bash -c "apt install -y libsodium-dev" > /dev/null
-docker exec -ti $MY_NAME bash -c "wget https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz; tar xf libsodium-1.0.17.tar.gz; cd libsodium-1.0.17; ./configure; make -j; make -j install"
+docker exec -ti $MY_NAME bash -c "set -ev;
+    wget https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz;
+    tar xf libsodium-1.0.17.tar.gz;
+    cd libsodium-1.0.17;
+    ./configure;
+    make -j;
+    make -j install" > /dev/null
 docker exec -ti $MY_NAME bash -c "cd $MY_TARGET_MOUNT; git clone https://github.com/herumi/cybozulib.git" > /dev/null
+# vim: set expandtab :
